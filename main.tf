f060a02603030cf5f2b4e7b959f2d9c772cd5768
skullleeep <skullleep@gmail.com>
2025-04-03 15:25:45 +0300

    Format files

--- main.tf
+++ main.tf
@@ -55,15 +55,15 @@ resource "azurerm_mssql_firewall_rule" "amfr" {
 
 # Create MSSQL Database
 resource "azurerm_mssql_database" "db" {
-  name         = var.sql_database_name
-  server_id    = azurerm_mssql_server.sqlserver.id
-  collation    = "SQL_Latin1_General_CP1_CI_AS"
-  license_type = "LicenseIncluded"
-  max_size_gb  = 1
-  sku_name     = "S0"
-  zone_redundant = false
+  name                 = var.sql_database_name
+  server_id            = azurerm_mssql_server.sqlserver.id
+  collation            = "SQL_Latin1_General_CP1_CI_AS"
+  license_type         = "LicenseIncluded"
+  max_size_gb          = 1
+  sku_name             = "S0"
+  zone_redundant       = false
   storage_account_type = "Zone"
-  geo_backup_enabled = false
+  geo_backup_enabled   = false
 }
 
 # Create the web app
@@ -80,8 +80,8 @@ resource "azurerm_linux_web_app" "alwa" {
     always_on = false
   }
   connection_string {
-    name = "DefaultConnection"
-    type = "SQLAzure"
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
     value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,54 +1,54 @@
 variable "azure_subscription_id" {
-  type = string
+  type        = string
   description = "This is Azure's account subscription ID"
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "The name of the Resource Group"
 }
 
 variable "resource_group_location" {
-  type = string
+  type        = string
   description = "The world location of the Resource Group"
 }
 
 variable "app_service_plan_name" {
-  type = string
+  type        = string
   description = "The name of the Azure App Service Plan"
 }
 
 variable "app_service_name" {
-  type = string
+  type        = string
   description = "The name of the App"
 }
 
 variable "sql_server_name" {
-  type = string
+  type        = string
   description = "The name of the Azure MSSQL Server"
 }
 
 variable "sql_database_name" {
-  type = string
+  type        = string
   description = "The name of the Database in the MSSQL Server"
 }
 
 variable "sql_admin_login" {
-  type = string
+  type        = string
   description = "The username of the MSSQL Server Admin"
 }
 
 variable "sql_admin_password" {
-  type = string
+  type        = string
   description = "The password of the MSSQL Server Admin"
 }
 
 variable "firewall_rule_name" {
-  type = string
+  type        = string
   description = "The name of the Firwall Rule for the MSSQL Server"
 }
 
 variable "repo_URL" {
-  type = string
+  type        = string
   description = "The URL of the github repo that contains the project"
 }
