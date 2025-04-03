8b549bc91f9c6bb2d2abd12e12ac65de17d42b6f
skullleeep <skullleep@gmail.com>
2025-04-03 15:18:51 +0300

    Initialize

---
+++ main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  subscription_id = var.azure_subscription_id
+}
+
+# Generate random integer to create unique name globally
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create resource group
+resource "azurerm_resource_group" "arg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "asp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+# Create MSSQL Server
+resource "azurerm_mssql_server" "sqlserver" {
+  name                         = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.arg.name
+  location                     = azurerm_resource_group.arg.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+# Create MSSQL Server Firewall Rule
+resource "azurerm_mssql_firewall_rule" "amfr" {
+  name             = var.firewall_rule_name
+  server_id        = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+# Create MSSQL Database
+resource "azurerm_mssql_database" "db" {
+  name         = var.sql_database_name
+  server_id    = azurerm_mssql_server.sqlserver.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 1
+  sku_name     = "S0"
+  zone_redundant = false
+  storage_account_type = "Zone"
+  geo_backup_enabled = false
+}
+
+# Create the web app
+resource "azurerm_linux_web_app" "alwa" {
+  name                = var.app_service_name
+  resource_group_name = azurerm_resource_group.arg.name
+  location            = azurerm_resource_group.arg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+  connection_string {
+    name = "DefaultConnection"
+    type = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+# Deploy code from Github repo
+resource "azurerm_app_service_source_control" "aassc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = var.repo_URL
+  branch                 = "main"
+  use_manual_integration = true
+}
